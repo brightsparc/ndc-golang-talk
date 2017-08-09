0acf1437a85a0a29e2636a063b9e4583209ae37c
Julian Bright <brightsparc@gmail.com>
2017-08-10 00:26:45 +1000

    Adding writekey to file

--- ndc_segment/track.sh
+++ ndc_segment/track.sh
@@ -1,8 +1,8 @@
 # Make sure we have created the delivery stream
-aws firehose --endpoint http://localhost:4573 \
-  create-delivery-stream --delivery-stream-name=test-stream
+aws firehose --endpoint http://localhost:4573 create-delivery-stream --delivery-stream-name=test-stream
 
 # Send track event to API
-curl -u 8GT90rhOugcEGAqdcBWP8muss7iSwwHy: \
-  -H "Content-Type: application/json" -d @track.json \
-  -X POST ${1-https://api.segment.io/v1/track}
+curl -H "Content-Type: application/json" -d @track.json -X POST ${1-https://api.segment.io/v1/t}
+
+# send multiple requests
+hey -n 100 -c 10 -H "Content-Type: application/json" -D ./track.json -m POST ${1-https://api.segment.io/v1/t}
