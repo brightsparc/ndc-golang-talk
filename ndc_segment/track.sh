7791a308088a7aae3d1d6fff4543be502a60061a
Julian Bright <brightsparc@gmail.com>
2017-08-10 11:49:36 +1000

    Adding prometheus configuration and docker compose

--- ndc_segment/track.sh
+++ ndc_segment/track.sh
@@ -2,4 +2,4 @@
 curl -H "Content-Type: application/json" -d @track.json -X POST ${1-https://api.segment.io/v1/t}
 
 # send multiple requests
-hey -n 100 -c 10 -H "Content-Type: application/json" -D ./track.json -m POST ${1-https://api.segment.io/v1/t}
+hey -n 1000 -c 10 -H "Content-Type: application/json" -D ./track.json -m POST ${1-https://api.segment.io/v1/t}
