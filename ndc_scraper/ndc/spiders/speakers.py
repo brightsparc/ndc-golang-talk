b7aa5e8dafa017e76e90019b95b38489d2f20021
Julian Bright <brightsparc@gmail.com>
2017-08-13 18:08:01 +1000

    Updating scraper to nest speaker and talk, outputing the order for lightning talks and including date/time/venue attributes

--- ndc_scraper/ndc/spiders/speakers.py
+++ ndc_scraper/ndc/spiders/speakers.py
@@ -10,7 +10,7 @@ class SpeakersSpider(scrapy.Spider):
 
     def parse(self, response):
         for speaker in response.css("a.boxed-speaker::attr(href)").extract():
-            next_page = response.urljoin(speaker)
+            next_page = response.urljoin(speaker).strip('/') # Strip trailing slash
             yield scrapy.Request(next_page, callback=self.parse_speaker)
 
     def strip_not_empty(self, text):
@@ -22,33 +22,45 @@ class SpeakersSpider(scrapy.Spider):
     def parse_speaker(self, response):
         item = {
             "conference": response.css("div.logo p::text").re("\w+")[0],
-            "name": self.strip_not_empty(response.css("section.masthead h1::text").extract_first()),
-            "tagline": self.strip_not_empty(response.css("section.masthead h1 span::text").extract_first()),
-            "image": response.urljoin(response.css("section.masthead img::attr(src)").extract_first()),
-            "handle": self.strip_not_empty(response.css("section.masthead a::text").extract_first()),
-            "preamble": self.join_not_empty(response.css("section.preamble p::text").extract()),
+            "speaker": {
+                "url": response.url,
+                "name": self.strip_not_empty(response.css("section.masthead h1::text").extract_first()),
+                "tagline": self.strip_not_empty(response.css("section.masthead h1 span::text").extract_first()),
+                "imageUrl": response.urljoin(response.css("section.masthead img::attr(src)").extract_first()),
+                "handle": self.strip_not_empty(response.css("section.masthead a::text").extract_first()),
+                "preamble": self.join_not_empty(response.css("section.preamble p::text").extract()),
+            }
         }
 
-        for talk in response.css("section.events li a::attr(href)").extract():
-            next_page = response.urljoin(talk)
-            yield scrapy.Request(next_page, callback=self.parse_talk, meta={"item": item})
+        for talk in response.css("section.events li a"):
+            link = response.urljoin(talk.css("::attr(href)").extract_first())
+            item["talk"] = {
+                "url": link,
+                "workshop": re.search("/workshop/", link) is not None,
+                "title": talk.css("h2::text").extract_first(),
+            }
+            # Reqeust the talk, but don't filter as we want to get multiple talks from the different speakers
+            yield scrapy.Request(link, callback=self.parse_talk, dont_filter=True, meta={"item": item})
 
     def parse_talk(self, response):
         item = response.meta["item"]
+        talk = item["talk"]
         print(response.url)
+        # Get speaker links, if more than one pass through the 1-based order
+        speaker_links = [response.urljoin(link) for link in response.css("section.speakers li a::attr(href)").extract()]
+        if speaker_links and len(speaker_links) > 0:
+            try:
+                talk["order"] = speaker_links.index(item["speaker"]["url"]) + 1
+                print('Got speaker order', talk["order"], speaker_links)
+            except ValueError as e:
+                print("Error getting speaker order", e)
+        # Get tags excluding level
         tags = response.css("section.masthead div.tags a::text").extract()
-        levels = [s[7:] for s in tags if s.startswith('Level: ')]
-        if not levels:
-            level = 'All levels'
-        else:
-            level = levels[0]
-        item["talk"] = {
-            "workshop": re.search("/workshop/", response.url) is not None,
-            "url": response.url,
-            "level": level,
-            "title": self.strip_not_empty(response.css("section.masthead h1::text").extract_first()),
-            "tags": [s for s in tags if not s.startswith('Level: ')], # don't include levels
-            "preamble": self.join_not_empty(response.css("section.preamble p::text").extract()),
-            "body": self.join_not_empty(response.css("section.body p::text").extract()),
-        }
+        talk["tags"] = [s for s in tags if not s.startswith('Level: ')]
+        talk["preamble"] = self.join_not_empty(response.css("section.preamble p::text").extract())
+        talk["body"] = self.join_not_empty(response.css("section.body p::text").extract())
+        # Get day/time/venue/level attributes
+        for detail in response.css("section.details p"):
+            label = detail.css("p::text").extract_first().strip().lower()
+            talk[label] = detail.css("span::text").extract_first().strip()
         return item
