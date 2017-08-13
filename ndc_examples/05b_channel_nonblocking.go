1155d34d975a19a801f62bf86c08669f2c56d495
Julian Bright <brightsparc@gmail.com>
2017-08-13 11:23:55 +1000

    Updating slides for channel changes

--- ndc_examples/05b_channel_nonblocking.go
+++ ndc_examples/05b_channel_nonblocking.go
@@ -5,11 +5,14 @@ import (
 )
 
 func main() {
+	// Create channel to buffer single message
 	ch := make(chan string, 1)
 	select {
 	case ch <- "buffered":
 		fmt.Println("message 1 sent")
 	}
+
+	// Default case falls through without blocking
 	select {
 	case ch <- "non-blocking":
 		fmt.Println("message 2 sent")
